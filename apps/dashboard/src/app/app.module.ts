28da70dffa63b48a3d2e68d78706f8e4c540c4d3
Willian Silva <silva.williansantos@gmail.com>
2023-12-29 21:11:07 -0300

    feat: add create form

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
 // material
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatSidenavModule } from "@angular/material/sidenav";
@@ -9,15 +10,19 @@ import { MatListModule } from "@angular/material/list";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 // modules
 import { AppRoutingModule } from "@app/app-routing.module";
 import { AppComponent } from "@app/app.component";
+
 // components
 import { HeaderComponent } from "@components/template/header/header.component";
 import { FooterComponent } from "@components/template/footer/footer.component";
 import { NavComponent } from "@components/template/nav/nav.component";
 import { CreateComponent } from "@components/product/create/create.component";
+
 // views
 import { HomeComponent } from "@views/home/home.component";
 import { ProductComponent } from "@views/product/product.component";
@@ -37,12 +42,16 @@ import { ProductComponent } from "@views/product/product.component";
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    FormsModule,
+    // material
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
     MatCardModule,
     MatButtonModule,
     MatSnackBarModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
